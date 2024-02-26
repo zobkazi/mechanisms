0feff3a22e1001de55553598af1b654003079253
zobaidulkazi64 <zobaidulkazi64.official@gmail.com>
2024-02-26 19:48:40 +0600

    express server with typescript setup and update

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import express, { Request, Response } from 'express';
+
+const app = express();
+const PORT = 3000;
+
+app.get('/', (req: Request, res: Response) => {
+  res.send('Hello, World!');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
