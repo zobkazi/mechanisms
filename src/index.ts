311902211f78e017296f8b6e3f5aebddb9218869
zobaidul kazi <zobaidulkazi64.official@gmail.com>
2024-05-11 22:57:59 +0600

    jwt auth middleware

--- src/index.ts
+++ src/index.ts
@@ -1,28 +1,26 @@
-import express from 'express';
-import mongoose from 'mongoose';
-import userRoutes from './routes/';
-import authMiddleware from './middleware/authMiddleware';
+import express from "express";
+import mongoose from "mongoose";
+import userRoutes from "./routes/";
+import authMiddleware from "./middleware/authMiddleware";
 
 const app = express();
 
 app.use(express.json());
 
-app.use('/api/users', userRoutes);
+app.use("/api/users", userRoutes);
 
-// // Protected route example
-// app.get('/api/profile', authMiddleware, (req, res) => {
-//   // Access userId from req.userData
-//   const userId = req['userData'].userId;
-//   res.status(200).json({ message: `Profile data for user with ID: ${userId}` });
-// });
+// Protected route example
+app.get("/api/profile", authMiddleware, (req, res) => {
+  // Access userId from req.userData
+  const userId = req["userData"].userId;
+  res.status(200).json({ message: `Profile data for user with ID: ${userId}` });
+});
 
 mongoose
-  .connect('mongodb://localhost:27017/your_database', {
-
-  })
+  .connect("mongodb://localhost:27017/your_database", {})
   .then(() => {
     app.listen(3000, () => {
-      console.log('Server is running on port 3000');
+      console.log("Server is running on port 3000");
     });
   })
   .catch((error) => console.error(error));
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import { register, login } from '../controller/create';
-import authMiddleware from '../middleware/authMiddleware';
+import express from "express";
+import { register, login } from "../controller/create";
+import authMiddleware from "../middleware/authMiddleware";
 
 const router = express.Router();
 
-router.post('/register', register);
-router.post('/login', login);
+router.post("/register", register);
+router.post("/login", login);
 
 export default router;
